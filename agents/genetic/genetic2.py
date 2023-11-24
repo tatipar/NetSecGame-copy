dd9a67afae37cd9a7b82ce9f4c7ce42b55ba6d46
tatipar <tsparlanti@gmail.com>
2023-11-24 18:16:07 +0000

    some ideas (not working)

---
+++ agents/genetic/genetic2.py
@@ -0,0 +1,219 @@
+import sys
+from os import path
+# script:
+sys.path.append( path.dirname(path.dirname( path.dirname( path.abspath(__file__) ) ) ))
+# terminal:
+#sys.path.append(path.dirname(path.dirname(path.abspath(''))))
+
+import random
+import numpy as np
+import copy
+import argparse
+from env.network_security_game import NetworkSecurityEnvironment
+from agents.random.random_agent import RandomAgent
+from env.game_components import Action, ActionType, GameState #, IP, Data, Network, Service
+
+
+def individual_init(env, args, observation, size):
+    individual = []
+    random_agent = RandomAgent(env,args)
+    for i in range(size):
+        new_action = random_agent.move(observation, individual)
+        individual.append(new_action)
+        observation = env.step(new_action)
+    return individual
+
+
+def generate_valid_actions(state):
+    # (function copied from RandomAgent)
+    valid_actions = set()
+    #Network Scans
+    for network in state.known_networks:
+        # TODO ADD neighbouring networks
+        valid_actions.add(Action(ActionType.ScanNetwork, params={"target_network": network}))
+    # Service Scans
+    for host in state.known_hosts:
+        valid_actions.add(Action(ActionType.FindServices, params={"target_host": host}))
+    # Service Exploits
+    for host, service_list in state.known_services.items():
+        for service in service_list:
+            valid_actions.add(Action(ActionType.ExploitService, params={"target_host": host , "target_service": service}))
+    # Data Scans
+    for host in state.controlled_hosts:
+        valid_actions.add(Action(ActionType.FindData, params={"target_host": host}))
+    # Data Exfiltration
+    for src_host, data_list in state.known_data.items():
+        for data in data_list:
+            for trg_host in state.controlled_hosts:
+                if trg_host != src_host:
+                    valid_actions.add(Action(ActionType.ExfiltrateData, params={"target_host": trg_host, "source_host": src_host, "data": data}))
+    return list(valid_actions)
+
+
+def individual_id(individual, dict_ind):
+    len_dict = len(dict_ind)
+    dict_ind[str(individual)] = len_dict + 1
+    return dict_ind
+
+
+def save_states(individual, dict_ind, dict_states, observation):
+    idd = dict_ind[str(individual)]
+    dict_states[idd] = {}
+    for i in range(max_number_steps):
+        dict_states[idd][i] = observation.state.as_json()
+        observation = env.step(individual[i])
+    return dict_states
+
+
+def get_state(individual, dict_ind, dict_states, index): 
+    idd = dict_ind[str(individual)]
+    return GameState.from_json(dict_states[idd][index])
+
+    
+def mutation_operator_1gen(individual, env, index):
+    observation = env.reset()
+    new_action = []
+    for i in range(len(individual)):
+        if i < index:
+            valid_actions = generate_valid_actions(observation.state)
+            if individual[i] in valid_actions:
+                observation = env.step(individual[i])
+            else:
+                index = i
+        else:
+            break
+    valid_actions = generate_valid_actions(observation.state)
+    new_action.append(random.choice(valid_actions))
+    return individual[:index] + new_action + individual[index+1:]
+
+
+def mutation_operator(individual, observation, mutation_prob):
+    new_individual = []
+    for i in range(len(individual)):
+        if random.random() < mutation_prob:
+            valid_actions = generate_valid_actions(observation.state)
+            new_action = random.choice(valid_actions) 
+        else:
+            new_action = individual[i]
+        observation = env.step(new_action)
+        new_individual.append(new_action)
+    return new_individual
+
+
+def repair_operator2(individual, observation):
+    for i in range(len(individual)):
+        valid_actions = generate_valid_actions(observation.state)
+        if individual[i] in valid_actions:
+            pass
+        else:
+            individual[i] = random.choice(valid_actions)
+        observation = env.step(individual[i])
+    return individual
+
+
+def fitness_eval_ra(individual, observation, goal): #action_id, new_observation, x, goal, pre_validate, index):
+    reward = 0
+    for i in range(len(individual)):
+        previous_state = observation.state
+        valid_actions = generate_valid_actions(previous_state)
+    return new_individual
+
+
+def repair_operator(individual, observation):
+    for i in range(len(individual)):
+        valid_actions = generate_valid_actions(observation.state)
+        if individual[i] in valid_actions:
+            pass
+        else:
+            individual[i] = random.choice(valid_actions)
+        observation = env.step(individual[i])
+    return individual
+
+
+def fitness_eval_ra(individual, observation, goal): #action_id, new_observation, x, goal, pre_validate, index):
+    reward = 0
+    for i in range(len(individual)):
+        previous_state = observation.state
+        valid_actions = generate_valid_actions(previous_state)
+        if individual[i] in valid_actions:
+            observation = env.step(individual[i])
+            current_state = observation.state
+            if current_state != previous_state:
+                reward += observation.reward * (-10) 
+            else:
+                reward += 0
+        else:
+            reward += observation.reward * 100
+        if observation.done:
+            reward += observation.reward * (-1000)
+            break
+    final_reward = reward * i 
+    return final_reward
+
+    
+def choose_parents(population):
+    """ Tournament selection """
+    options = []
+    chosen = []
+    for i in range(2):
+        options.append(random.choice(population))
+        options.append(random.choice(population))
+        chosen.append(max(options, key=lambda x:fitness_eval_ra(x,env)))
+        options = []
+    return chosen[0], chosen[1]
+
+
+env = NetworkSecurityEnvironment("netsecenv-task.yaml")
+goal = copy.deepcopy(env._goal_conditions)
+#observation = env.reset()
+parser = argparse.ArgumentParser()
+parser.add_argument("--force_ignore", help="Force ignore repeated actions in code", default=False, action=argparse.BooleanOptionalAction)
+args = parser.parse_args()
+
+max_number_steps = 20
+
+# GA parameters
+population_size = 100
+num_generations = 100
+mutation_prob = 0.1
+cross_prob = 0.8
+num_replace = 30
+
+# Initialize population
+#population = [individual_init(env,args,max_number_steps) for _ in range(population_size)]
+population = []
+for _ in range(population_size):
+    observation = env.reset()
+    population.append(individual_init(env,args,observation,max_number_steps))
+
+# Generations
+generation = 0
+while (generation < num_generations):# or (best_score < 10000):
+    generation += 1
+    new_generation = []
+    offspring = []
+    for _ in range(int(population_size/2)):
+        # cross-over
+        parent1, parent2 = choose_parents(population)
+        if random.random() < cross_prob:
+            cross_point = random.randint(1, max_number_steps - 1)
+            son1 = parent1[:cross_point] + parent2[cross_point:]
+            son2 = parent2[:cross_point] + parent1[cross_point:]
+        else:
+            son1 = parent1.copy()
+            son2 = parent2.copy()
+        offspring.append(son1)
+        offspring.append(son2)
+    for j in range(population_size):
+        observation = env.reset()
+        # mutation:
+        population[j] = mutation_operator(population[j], observation, mutation_prob)
+        observation = env.reset()
+        population[j] = repair_operator(population[j], observation)
+    # steady-state
+    parents_scores = []
+    offspring_scores = []
+    for i in range(population_size):
+        observation = env.reset()
+        parents_scores.append(fitness_eval_ra(population[i], observation, goal))
+        observation = env.reset()
